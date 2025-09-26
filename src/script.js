8f1475d57a024b64dc0faed7eefda23ed0676dea
zeedro <zeedro53@gmail.com>
2025-09-26 04:12:20 +0100

    Replace dark mode button with toggle switch

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ document.getElementById('clickMe').addEventListener('click', () => {
   alert('Button was clicked!');
 });
 
-const toggleButton = document.getElementById("theme-toggle");
+const toggleSwitch = document.getElementById("theme-toggle");
 const body = document.body;
 
-toggleButton.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
+toggleSwitch.addEventListener("change", () => {
+  body.classList.toggle("dark-mode", toggleSwitch.checked);
 });
